ac9e139d945f04e499e88273d58cfe263608d17d
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-12-31 16:26:20 +0100

    feat(): searching speakers by query working

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Speaker } from "../Speaker/Speaker";
 import { SearchBar } from "../SpeakersSearchBar/SearchBar";
 import { speakers } from "./speakersData";
 
 const Speakers = () => {
+  const [searchQuery, setSearchQuery] = useState("")
+
   return (
     <section>
-      <SearchBar />
+      <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-12">
-        {speakers.map((speaker) => (
+        {speakers
+        .filter(({firstName, lastName}) => {
+          const targetResult = `${firstName} ${lastName}`.toLowerCase();
+          return (!searchQuery.trim() ? true : targetResult.includes(searchQuery.trim().toLowerCase()))
+        })
+        .map((speaker) => (
           <Speaker key={speaker.id} {...speaker} />
         ))}
       </div>
--- src/components/SpeakersSearchBar/SearchBar.js
+++ src/components/SpeakersSearchBar/SearchBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const SearchBar = () => {
+export const SearchBar = ({searchQuery, setSearchQuery}) => {
   return (
     <div className="mb-6 ">
       <input
@@ -8,6 +8,8 @@ export const SearchBar = () => {
         id="username"
         type="text"
         placeholder="Search by name"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
       />
     </div>
   );
